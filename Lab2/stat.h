574f54397c48387594f677a2b3835ae683ff2eac
Anastasia-Student <91686158+Anastasia-Student@users.noreply.github.com>
2021-12-10 15:33:18 +0200

    added bigrams counting functions with intersections

--- Lab2/stat.h
+++ Lab2/stat.h
@@ -35,3 +35,15 @@ map<Bigram, Frequancy> bigramFrequency(const string& text, int lenght = -1)  // 
 		frequancy.second.frequancy = frequancy.second.number / (double)lenght;  // Расчет частоты
 	return frequencies;
 }
+
+map<Bigram, Frequancy> bigramFrequencyWithIntersections(const string& text, int lenght = -1)  // Подсчет количества биграмм с пересечением
+{
+	if (lenght == -1 || lenght > (int)text.size())
+		lenght = (int)text.size();
+	map<Bigram, Frequancy> frequencies;
+	for (int i = 0; i < lenght - 1; i++)  // Смещение на одну букву
+		frequencies[Bigram(text[i], text[i + 1])].number++;  // Подсчет
+	for (auto& frequancy : frequencies)
+		frequancy.second.frequancy = frequancy.second.number / (double)lenght;  // Расчет частоты
+	return frequencies;
+}
