485f0f70e4810381ac574e3624230eea04298c15
Anastasia-Student <91686158+Anastasia-Student@users.noreply.github.com>
2021-12-11 15:53:46 +0200

    Added criterion 2.0

---
+++ Lab2/criterions.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "stat.h"
+
+#include <algorithm>
+
+using namespace std;
+
+struct Criterion20Result  // Структура результата критерия 2.0
+{
+	bool h1 = false;  // Гипотеза H1
+	int kh = 0;  // Kh
+	string unit;  // Биграмма или буква
+};
+
+template <typename Type>
+Criterion20Result criterion20(const string& text, // Критерий 2.0
+	const map<Type, Frequancy>& counters, int kh = 5, int lenght = -1)
+{
+	Criterion20Result result;
+	result.kh = kh;
+	if (lenght < 0 || lenght >(int)text.size())
+		lenght = (int)text.size();
+	vector<pair<Type, Frequancy>> afrg(counters.size());  // Afrg
+	for (auto counter : counters)  // Копирование map в vector
+		afrg.push_back(counter);  // Копирование map в vector
+	sort(afrg.begin(), afrg.end(), [](const pair<Type, Frequancy>& a,  // Сортировка по частоте
+		const pair<Type, Frequancy>& b) -> bool { return a.second.frequancy > b.second.frequancy; });
+
+	for (int i = 0; i < kh; i++)  // Первые Kh биграммы/буквы по частоте
+	{
+		if (text.find(afrg[i].first) == -1)  // Если текст содержит Afrg_i
+		{
+			result.unit = afrg[i].first;
+			result.h1 = true;  // Гипотеза H1 верна
+			break;
+		}
+	}
+
+	return result;
+}
